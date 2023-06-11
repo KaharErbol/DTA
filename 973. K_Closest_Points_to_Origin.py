fc04935ec2244acfdc3ed259c853da5b1215dc72
Kahar Erbol <kaharerbol@gmail.com>
2023-06-10 21:20:31 -0700

    Added 2nd solution - Heapq used

--- 973. K_Closest_Points_to_Origin.py
+++ 973. K_Closest_Points_to_Origin.py
@@ -6,3 +6,14 @@ class Solution:
             min_heap.append((distances[i], points[i]))
         min_heap.sort()
         return [ min_heap[i][1] for i in range(k)]
+
+
+### 2nd Solution used heapq
+# class Solution:
+#     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+#         min_heap = []
+#         for i in range(len(points)):
+#             dst = (points[i][0] ** 2 + points[i][1] ** 2) ** 0.5
+#             heapq.heappush(min_heap, (dst, points[i]))
+        
+#         return [ heapq.heappop(min_heap)[1] for _ in range(k)]
