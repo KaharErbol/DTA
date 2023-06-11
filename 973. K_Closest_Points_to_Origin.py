418b03a954f27270f6fc4f0d5e1012e2e93f184d
Kahar Erbol <kaharerbol@gmail.com>
2023-06-10 20:37:01 -0700

    updated the code

--- 973. K_Closest_Points_to_Origin.py
+++ 973. K_Closest_Points_to_Origin.py
@@ -5,7 +5,4 @@ class Solution:
         for i in range(len(points)):
             min_heap.append((distances[i], points[i]))
         min_heap.sort()
-        result = []
-        for i in range(k):
-            result.append(min_heap[i][1])
-        return result
+        return [ min_heap[i][1] for i in range(k)]
