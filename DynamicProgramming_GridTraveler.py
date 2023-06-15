c1095791a9b5a5aeba24172118c9564c28f73439
Kahar Erbol <kaharerbol@gmail.com>
2023-06-15 12:15:58 -0700

    Dynamic Programming - Grid Traveler

---
+++ DynamicProgramming_GridTraveler.py
@@ -0,0 +1,12 @@
+
+def grid_traveler(m,n, memo = {}):
+    key = (m,n)
+    if key in memo: return memo[key]
+    if m == 0 or n == 0: return 0
+    if m == 1 and n == 1: return 1
+    memo[key] = grid_traveler(m-1, n, memo) + grid_traveler(m, n-1, memo)
+    return memo[key]
+
+res = grid_traveler(18,18)
+print(res)
+    
--- DynamicProgramming_fibonacci.py
+++ DynamicProgramming_fibonacci.py
@@ -1,10 +1,9 @@
-memo = {}
-def fib(n):
+def fib(n, memo = {}):
     if n in memo: return memo[n]
     if n == 0: return 0
     if n == 1: return 1
 
-    memo[n] = fib(n-1) + fib(n-2)
+    memo[n] = fib(n-1, memo) + fib(n-2, memo)
 
     return memo[n]
 
