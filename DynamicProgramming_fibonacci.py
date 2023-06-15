ff14e795203e18dad7a6473ca3ed74a3e6d37584
Kahar Erbol <kaharerbol@gmail.com>
2023-06-15 12:09:25 -0700

    Dynamic Programming - Fibonacci Number

---
+++ DynamicProgramming_fibonacci.py
@@ -0,0 +1,12 @@
+memo = {}
+def fib(n):
+    if n in memo: return memo[n]
+    if n == 0: return 0
+    if n == 1: return 1
+
+    memo[n] = fib(n-1) + fib(n-2)
+
+    return memo[n]
+
+
+print(fib(99))
