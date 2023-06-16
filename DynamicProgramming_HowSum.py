ab458fb1c106dd31994b375986e1b313d17fa8b8
Kahar Erbol <kaharerbol@gmail.com>
2023-06-16 14:47:29 -0700

    Dynamic Programming - HowSum

---
+++ DynamicProgramming_HowSum.py
@@ -0,0 +1,21 @@
+"""
+How many ways the elements in the arr can generate the target value?
+"""
+
+def how_sum(target, numbers, memo={}):
+    if target in memo: return memo[target]
+    if target == 0: return []
+    if target < 0 : return None
+
+    for num in  numbers:
+        remainder = target - num
+        remainder_res = how_sum(remainder, numbers)
+        if remainder_res is not None:
+            remainder_res.append(num)
+            memo[target] = remainder_res
+            return remainder_res
+    memo[target] = None
+    return None
+
+print(how_sum(7, [2,3,4,5,7]))
+print(how_sum(300, [7, 14]))
