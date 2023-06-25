aa1f8fc6884ea9af241f3828b8551f51e2c02456
Kahar Erbol <kaharerbol@gmail.com>
2023-06-24 21:22:50 -0700

    LeetCode 322 Coin Change - DP Buttom Up Method

---
+++ 322_CoinChange.py
@@ -0,0 +1,20 @@
+def coin_change(coins, amount):
+    dp = [float('inf') for _ in range(amount+1)]
+    dp[0] = 0
+
+    for i in range(1, amount + 1):
+        for c in coins:
+            if c <= i:
+                dp[i] = min(dp[i], 1 + dp[i - c])
+
+    return -1 if dp[amount] == float('inf') else dp[amount]
+
+
+print(coin_change([1,2,5], 11))
+
+"""
+coins: 1, 2, 3
+amount: 4
+dp:     0   1   2   3   4   
+coins:  0   1   1   2   2
+"""
