222f2a0a865ea583453ceacea49db19918131f06
Kahar Erbol <kaharerbol@gmail.com>
2023-06-30 11:25:25 -0700

    Added 2nd approach - two pointers

--- 56_MergeIntervals.py
+++ 56_MergeIntervals.py
@@ -12,5 +12,28 @@ def merge(intervals):
 
 intervals = [[1,3],[2,6],[8,10],[15,18]]
 
+intervals_2 = [[1,3],[2,6],[8,10],[15,18]]
 
-print(merge(intervals))
+
+print("1: ", merge(intervals))
+
+def merge_2(intervals):
+    intervals.sort(key = lambda x: x[0])
+    start = intervals[0][0]
+    end = intervals[0][1]
+
+    res = []
+
+    for i in range(len(intervals)):
+        cur = intervals[i]
+        if cur[0] > end:
+            res.append([start, end])
+            start = cur[0]
+            end = cur[1]
+        else:
+            if cur[1] > end:
+                end = cur[1]
+    res.append([start, end])
+    return res
+
+print("2: ", merge_2(intervals_2))
