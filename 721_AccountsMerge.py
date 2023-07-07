0567281942eb97d13a74dcca1945ce9ef07a38db
KaharErbol <kaharerbol@gmail.com>
2023-07-07 14:01:35 -0700

    Union-Find Solution

--- 721_AccountsMerge.py
+++ 721_AccountsMerge.py
@@ -1,18 +1,50 @@
-def accounts_merge(accounts):
-    store = {}
-    res = []
-    for each in accounts:
-        if each[0] not in store:
-            store[each[0]] = set(each[1:])
+class UnionFind:
+    def __init__(self, N):
+        self.par = [i for i in range(N)]
+        self.rank = [1] * N
+    
+    def find(self, x):
+        while x != self.par[x]:
+            self.par[x] = self.par[self.par[x]]
+            x = self.par[x]
+        return x
+
+    def union(self, x1, x2):
+        p1, p2 = self.find(x1), self.find(x2)
+        if p1 == p2:
+            return False
+        if self.rank[p1] > self.rank[p2]:
+            self.par[p2] = p1
+            self.rank[p1] += self.rank[p2]
         else:
-            for mail in each[1:]:
-                if mail not in store[each[0]]:
-                    store[each[0]].add(mail)
-    for name, emails in store.items():
-        res.append([name] + sorted(emails))
-    return res
+            self.par[p1] = p2
+            self.rank[p2] += self.rank[p1]
+        return True
+
+class Solution:
+    def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
+        uf = UnionFind(len(accounts))
+        email_hash = {}
+        for i, a in enumerate(accounts):
+            for e in a[1:]:
+                if e in email_hash:
+                    uf.union(i, email_hash[e])
+                else:
+                    email_hash[e] = i
+        
+        ans = {}
+
+        for e, a_idx in email_hash.items():
+            leader = uf.find(a_idx)
+            if leader not in ans:
+                ans[leader] = []
+            ans[leader].append(e)
+
+        res = []
+        for idx, emails in ans.items():
+            acc_name = accounts[idx][0]
+            res.append([acc_name] + sorted(emails))
+        
+        return res
 
 
-
-accounts = [["John","johnsmith@mail.com","john_newyork@mail.com"],["John","johnsmith@mail.com","john00@mail.com"],["Mary","mary@mail.com"],["John","johnnybravo@mail.com"]]
-print(accounts_merge(accounts))
