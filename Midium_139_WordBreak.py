46d8a458809d6b5e930ddbac348425d3d24ea5b4
KaharErbol <kaharerbol@gmail.com>
2023-07-11 11:27:20 -0700

    DP Approach

---
+++ Midium_139_WordBreak.py
@@ -0,0 +1,17 @@
+def word_break(s, wordDict):
+    dp = [False] * (len(s) + 1)
+    dp[len(s)] = True
+
+    for i in range(len(s) - 1, -1, -1):
+        for word in wordDict:
+            if (i + len(word)) <= len(s) and s[i : i + len(word)] == word:
+                dp[i] = dp[i + len(word)]
+            if dp[i]:
+                break
+
+    return dp[0]
+
+s = "aaaaaaa"
+wordDict = ["aaaa","aaa"]
+
+print(word_break(s, wordDict))
