d3e312079a312480751ff6e503ae6b2482c4b714
KaharErbol <kaharerbol@gmail.com>
2024-01-04 14:03:20 -0800

    EASY

--- 1512_NumOfGoodPairs.py
+++ 1512_NumOfGoodPairs.py
@@ -10,15 +10,16 @@ for n,c in count.items():
 print(res)
 
 
-def numIdenticalPairs(self, nums):
-        hash_map = {}
-        res = 0
-        for element in nums:
-            if element not in hash_map:
-                hash_map[element] = 0
-            hash_map[element] += 1
-        
-        for k,v in hash_map.items():
-            res += v * (v - 1) // 2
-    
-        return res
+def numIdenticalPairs(nums):
+    hash_table = {}
+    res = 0
+
+    for num in nums:
+        if num not in hash_table:
+            hash_table[num] = 0
+        hash_table[num] += 1
+
+    for val in hash_table.values():
+        res += val * (val - 1) // 2
+
+    return res
